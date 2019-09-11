0c2719dcfb1f405f2ab0ff8f6313d7f849fab161
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-09-11 06:28:10 +0100

    [ Áreas de actuación ] » Solucionado efecto indeseado en apertura de modal

--- wp-content/themes/enfold-child/functions.js
+++ wp-content/themes/enfold-child/functions.js
@@ -6,19 +6,34 @@ jQuery(document).ready(function($) {
 
 	// Apertura de modal
 	$("#areas .iconbox").click(function () {
+		
 		var clase = ".modal-" + $(this).find("h3").text().toLowerCase().replace(/ /g, "-");
-		$("#header").fadeOut();
-		$("#modales").fadeIn();
+		
 		$(clase).addClass("active").css({"opacity" : "1", "position" : "relative"});
 		$("html, body").css("overflow", "hidden");
+
+		$("#header").fadeOut(250);
+
+		setTimeout(function() {
+			$("#modales").fadeIn(250);
+		}, 250);
+
 	});
 
 	// Cierre de modal
 	$("#modales .cerrar-modal").click(function () {
-		$("#header").fadeIn();
-		$("#modales").fadeOut();
-		$("#modales .active").removeClass("active").css({"opacity" : "0", "position" : "absolute"});
-		$("html, body").css("overflow", "visible");
+		
+		$("#header").fadeIn(250);
+		
+		setTimeout(function() {
+			$("#modales").fadeOut(250);
+		}, 250);
+		
+		setTimeout(function() {
+			$("#modales .active").removeClass("active").css({"opacity" : "0", "position" : "absolute"});
+			$("html, body").css("overflow", "visible");
+		}, 500);
+
 	});
 
 	// ──────────────── //
